ed7be842a27c98b174b58eabb8e92fd894ef0a51
ChenYun <chenyun@gojiejiao.com>
2017-09-02 17:21:21 +0800

    反馈模块修改

--- src/com/yun/opern/controller/FeedbackController.java
+++ src/com/yun/opern/controller/FeedbackController.java
@@ -5,6 +5,7 @@ import com.yun.opern.model.feedback.CommitFeedbackReq;
 import com.yun.opern.service.FeedbackService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class FeedbackController {
     private FeedbackService feedbackService;
 
     @RequestMapping(value = "/commit", method = RequestMethod.POST)
-    public BaseResponse commitFeedback(CommitFeedbackReq request){
+    public BaseResponse commitFeedback(@RequestBody CommitFeedbackReq request){
         String methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
         logger.info(methodName + "--start");
         BaseResponse response = new BaseResponse();
